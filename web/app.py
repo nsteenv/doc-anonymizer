e846d545ec0cc1b21a4a4e80b066287a2abae6c2
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-07-27 14:57:09 +0200

    Move files from upload to processed folder

--- web/app.py
+++ web/app.py
@@ -1,30 +1,48 @@
 # app.py
 
-
-from flask import Flask
-from flask import request, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from config import BaseConfig
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
-db = SQLAlchemy(app)
 
-
-from models import *
-
+ALLOWED_EXTENSIONS = set(['doc', 'docx'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def upload_file():
     if request.method == 'POST':
-        text = request.form['text']
-        post = Post(text)
-        db.session.add(post)
-        db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('index.html', posts=posts)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if allowed_file(file.filename) is False:
+            flash('File format should be %s', ALLOWED_EXTENSIONS)
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # @TODO 
+            # process the file here
+            # then save it to app.config['PROCESSED_FOLDER']
+            file.save(os.path.join(app.config['PROCESSED_FOLDER'], filename))
+            return redirect(url_for('processed_file', filename=filename))
+    return render_template('index.html')
 
+@app.route('/processed/<filename>')
+def processed_file(filename):
+    return send_from_directory(app.config['PROCESSED_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run()
