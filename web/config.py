db072feaaa7baaa2d9c9f03dc889a151255cd03b
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-07-29 13:32:36 +0200

    Use original config

--- web/config.py
+++ web/config.py
@@ -3,15 +3,15 @@
 
 import os
 
-
-# class BaseConfig(object):
-#     SECRET_KEY = os.environ['SECRET_KEY']
-#     DEBUG = os.environ['DEBUG']
-#     UPLOAD_FOLDER = os.environ['UPLOAD_FOLDER']
-#     PROCESSED_FOLDER = os.environ['PROCESSED_FOLDER']
-
 class BaseConfig(object):
-    SECRET_KEY = "Hello"
-    DEBUG = True
-    UPLOAD_FOLDER = "input"
-    PROCESSED_FOLDER = "output"
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DEBUG = os.environ['DEBUG']
+    UPLOAD_FOLDER = os.environ['UPLOAD_FOLDER']
+    PROCESSED_FOLDER = os.environ['PROCESSED_FOLDER']
+
+# Debug mode
+# class BaseConfig(object):
+#     SECRET_KEY = "Hello"
+#     DEBUG = True
+#     UPLOAD_FOLDER = "input"
+#     PROCESSED_FOLDER = "output"
